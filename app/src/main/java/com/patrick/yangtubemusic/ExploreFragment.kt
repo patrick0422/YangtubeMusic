bc84d82845c11cac8c62fc72555752ca62de9436
양태웅 <didxodnd0422@gmail.com>
2022-08-23 19:29:49 +0900

    Integrate BottomNav with Nav Component

--- app/src/main/java/com/patrick/yangtubemusic/ExploreFragment.kt
+++ app/src/main/java/com/patrick/yangtubemusic/ExploreFragment.kt
@@ -1,18 +1,11 @@
 package com.patrick.yangtubemusic
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.patrick.yangtubemusic.base.BaseFragment
+import com.patrick.yangtubemusic.databinding.FragmentExploreBinding
 
 
-class ExploreFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+class ExploreFragment : BaseFragment<FragmentExploreBinding>(R.layout.fragment_explore) {
+    override fun init() {
+
     }
 }
--- app/src/main/java/com/patrick/yangtubemusic/LibraryFragment.kt
+++ app/src/main/java/com/patrick/yangtubemusic/LibraryFragment.kt
@@ -1,18 +1,11 @@
 package com.patrick.yangtubemusic
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.patrick.yangtubemusic.base.BaseFragment
+import com.patrick.yangtubemusic.databinding.FragmentLibraryBinding
 
 
-class LibraryFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false)
+class LibraryFragment : BaseFragment<FragmentLibraryBinding>(R.layout.fragment_library) {
+    override fun init() {
+
     }
 }
