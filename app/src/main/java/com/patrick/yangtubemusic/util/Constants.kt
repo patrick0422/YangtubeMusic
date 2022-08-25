3d594527479f58d7c7b0f29f2f5706047340c788
양태웅 <didxodnd0422@gmail.com>
2022-08-26 08:06:11 +0900

    Add dummy recent activity data

--- app/src/main/java/com/patrick/yangtubemusic/util/Constants.kt
+++ app/src/main/java/com/patrick/yangtubemusic/util/Constants.kt
@@ -6,6 +6,53 @@ import com.patrick.yangtubemusic.data.Music
 object Constants {
     const val TAG = "YangtubeMusic"
 
+    val mockRecentActivityList = listOf(
+        Music(
+            "내 기쁨은 네가 벤틀리를 타는 거야",
+            "김승민",
+            "https://lh3.googleusercontent.com/yZTtpy3PXhUx6BaUl-SxiDDqWvI-VfAGHvGpUfQOosEMqMstR10_9HecHd1u0w0vNrota6BGNk_HDg4=w120-h120-l90-rj",
+            true
+        ),
+        Music(
+            "Me & You ◑",
+            "HONNE(혼네)",
+            "https://lh3.googleusercontent.com/XnLiu9JAt_Z4GDTeLlVQ1EVgFXS1xdQcVRDeh83whiC5TQgmboVuQsKmQU0L8Bx7uhqpgrpfZnnl1HFZ=w120-h120-l90-rj",
+            false
+        ),
+        Music(
+            "니가 돌아올 희망은 없다는 걸 알아",
+            "경제환",
+            "https://lh3.googleusercontent.com/_At5xpPD0paZRRwN9KOLpnvc2yOwjWNVSBm6Es77cM6dC5sk15BKgsI9AQj8b97CyU-l1swqbOpPKkw=w120-h120-l90-rj"
+        ),
+        Music(
+            "Tomorrow(feat. 기리보이(Giriboy), 빅나티)",
+            "lIlBOI",
+            "https://lh3.googleusercontent.com/pixroKTrwwEDqLTP3wPA8ePRMvWBz4u4K_XVqeahLEEQ2ySdYXM_5x-qIt6PKB9S6eHzw_3vKcOURlCVCw=w120-h120-l90-rj"
+        ),
+        Music(
+            "내 기쁨은 네가 벤틀리를 타는 거야",
+            "김승민",
+            "https://lh3.googleusercontent.com/yZTtpy3PXhUx6BaUl-SxiDDqWvI-VfAGHvGpUfQOosEMqMstR10_9HecHd1u0w0vNrota6BGNk_HDg4=w120-h120-l90-rj",
+            true
+        ),
+        Music(
+            "Me & You ◑",
+            "HONNE(혼네)",
+            "https://lh3.googleusercontent.com/XnLiu9JAt_Z4GDTeLlVQ1EVgFXS1xdQcVRDeh83whiC5TQgmboVuQsKmQU0L8Bx7uhqpgrpfZnnl1HFZ=w120-h120-l90-rj",
+            false
+        ),
+        Music(
+            "니가 돌아올 희망은 없다는 걸 알아",
+            "경제환",
+            "https://lh3.googleusercontent.com/_At5xpPD0paZRRwN9KOLpnvc2yOwjWNVSBm6Es77cM6dC5sk15BKgsI9AQj8b97CyU-l1swqbOpPKkw=w120-h120-l90-rj"
+        ),
+        Music(
+            "Tomorrow(feat. 기리보이(Giriboy), 빅나티)",
+            "lIlBOI",
+            "https://lh3.googleusercontent.com/pixroKTrwwEDqLTP3wPA8ePRMvWBz4u4K_XVqeahLEEQ2ySdYXM_5x-qIt6PKB9S6eHzw_3vKcOURlCVCw=w120-h120-l90-rj"
+        ),
+    )
+
     val mockList = listOf(
         arrayOf(
             Music(
