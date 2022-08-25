41b627789e19a004f20188b22fdb06ff872c725f
양태웅 <didxodnd0422@gmail.com>
2022-08-25 20:24:20 +0900

    Move LibraryFragment

--- app/src/main/java/com/patrick/yangtubemusic/presentation/library/LibraryFragment.kt
+++ app/src/main/java/com/patrick/yangtubemusic/presentation/library/LibraryFragment.kt
@@ -1,11 +1,13 @@
-package com.patrick.yangtubemusic
+package com.patrick.yangtubemusic.presentation.library
 
+import com.patrick.yangtubemusic.R
 import com.patrick.yangtubemusic.base.BaseFragment
 import com.patrick.yangtubemusic.databinding.FragmentLibraryBinding
 
 
 class LibraryFragment : BaseFragment<FragmentLibraryBinding>(R.layout.fragment_library) {
-    override fun init() {
 
+    override fun init() {
+        
     }
 }
