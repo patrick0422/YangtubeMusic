9128e63fe905e96b68b11e49d3774a6f175f6f59
양태웅 <didxodnd0422@gmail.com>
2022-08-25 14:30:57 +0900

    Create BottomSheetButton

---
+++ app/src/main/java/com/patrick/yangtubemusic/presentation/bottomsheet/BottomSheetButton.kt
@@ -0,0 +1,42 @@
+package com.patrick.yangtubemusic.presentation.bottomsheet
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.DrawableRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.patrick.yangtubemusic.R
+import com.patrick.yangtubemusic.databinding.ButtonBottomSheetBinding
+
+class BottomSheetButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val binding: ButtonBottomSheetBinding by lazy {
+        ButtonBottomSheetBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    @DrawableRes
+    var icon: Int
+    var description: String
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.BottomSheetButton,
+            0, 0).apply {
+
+            try {
+                icon = getResourceId(R.styleable.BottomSheetButton_icon, R.drawable.ic_question)
+                description = getString(R.styleable.BottomSheetButton_description) ?: "Unknown"
+
+                binding.imageIcon.setImageResource(icon)
+                binding.textDescription.text = description
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+}
