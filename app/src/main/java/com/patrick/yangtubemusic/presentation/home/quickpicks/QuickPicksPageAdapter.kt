71757521b67d968ca087327f27dc433b893bc65a
양태웅 <didxodnd0422@gmail.com>
2022-08-25 14:48:58 +0900

    Add onClickListener in QuickPicksPageAdapter

--- app/src/main/java/com/patrick/yangtubemusic/presentation/home/quickpicks/QuickPicksPageAdapter.kt
+++ app/src/main/java/com/patrick/yangtubemusic/presentation/home/quickpicks/QuickPicksPageAdapter.kt
@@ -2,15 +2,23 @@ package com.patrick.yangtubemusic.presentation.home.quickpicks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.patrick.yangtubemusic.data.Music
 import com.patrick.yangtubemusic.databinding.PageQuickPicksBinding
 
-class QuickPicksPageAdapter: RecyclerView.Adapter<QuickPicksPageViewHolder>() {
-    var quickPicksPageList: List<Array<MusicCompact>> = emptyList()
+class QuickPicksPageAdapter(
+    private val onItemClick: (Music) -> Unit,
+    private val onItemLongClick: (Music) -> Unit,
+    private val onMoreClick: (Music) -> Unit
+): RecyclerView.Adapter<QuickPicksPageViewHolder>() {
+    var quickPicksPageList: List<Array<Music>> = emptyList()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuickPicksPageViewHolder = QuickPicksPageViewHolder.from(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuickPicksPageViewHolder = QuickPicksPageViewHolder.from(
+        parent,
+        onItemClick,
+        onItemLongClick,
+        onMoreClick
+    )
 
     override fun onBindViewHolder(holder: QuickPicksPageViewHolder, position: Int) = holder.bind(quickPicksPageList[position])
 
@@ -18,20 +26,37 @@ class QuickPicksPageAdapter: RecyclerView.Adapter<QuickPicksPageViewHolder>() {
 }
 
 class QuickPicksPageViewHolder(
-    private val binding: PageQuickPicksBinding
+    private val binding: PageQuickPicksBinding,
+    private val onItemClick: (Music) -> Unit,
+    private val onItemLongClick: (Music) -> Unit,
+    private val onMoreClick: (Music) -> Unit
 ): RecyclerView.ViewHolder(binding.root) {
     companion object {
-        fun from(parent: ViewGroup): QuickPicksPageViewHolder = QuickPicksPageViewHolder(
+        fun from(
+            parent: ViewGroup,
+            onItemClick: (Music) -> Unit,
+            onItemLongClick: (Music) -> Unit,
+            onMoreClick: (Music) -> Unit
+        ): QuickPicksPageViewHolder = QuickPicksPageViewHolder(
             PageQuickPicksBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false
-            )
+            ),
+            onItemClick,
+            onItemLongClick,
+            onMoreClick
         )
     }
 
-    fun bind(musicList: Array<MusicCompact>) = with(binding) {
-        quickPicksItem1.musicData = musicList[0]
-        quickPicksItem2.musicData = musicList[1]
-        quickPicksItem3.musicData = musicList[2]
-        quickPicksItem4.musicData = musicList[3]
+    fun bind(musicList: Array<Music>) = with(binding) {
+        listOf(
+            quickPicksItem1, quickPicksItem2, quickPicksItem3, quickPicksItem4
+        ).forEachIndexed { index, quickPicksItem ->
+            with(quickPicksItem) {
+                musicData = musicList[index]
+                setOnClickListener { onItemClick(it) }
+                setOnLongClickListener { onItemLongClick(it) }
+                setOnMoreClickListener { onMoreClick(it) }
+            }
+        }
     }
 }
