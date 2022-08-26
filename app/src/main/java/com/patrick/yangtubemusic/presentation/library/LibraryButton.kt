730d88eadf1785a31818f53aa2d185d03bdf8b4b
양태웅 <didxodnd0422@gmail.com>
2022-08-26 14:18:54 +0900

    Create LibraryButton

---
+++ app/src/main/java/com/patrick/yangtubemusic/presentation/library/LibraryButton.kt
@@ -0,0 +1,43 @@
+package com.patrick.yangtubemusic.presentation.library
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.DrawableRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.patrick.yangtubemusic.R
+import com.patrick.yangtubemusic.databinding.ButtonLibraryBinding
+
+class LibraryButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val binding: ButtonLibraryBinding by lazy {
+        ButtonLibraryBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    @DrawableRes
+    var icon: Int
+    var description: String
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs, R.styleable.Common, 0, 0
+        ).apply {
+            try {
+                icon = getResourceId(R.styleable.Common_icon, R.drawable.ic_question)
+                description = getString(R.styleable.Common_description) ?: "Unknown"
+
+                binding.imageIcon.setImageResource(icon)
+                binding.textDescription.text = description
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    fun setOnClickListener(onClick: () -> Unit) {
+        binding.layout.setOnClickListener { onClick() }
+    }
+}
