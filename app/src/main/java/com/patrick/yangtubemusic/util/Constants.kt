851bdaf58c68aa78527da00d97a080e6b7a93e8b
양태웅 <didxodnd0422@gmail.com>
2022-08-26 16:16:17 +0900

    Apply minor changes

--- app/src/main/java/com/patrick/yangtubemusic/util/Constants.kt
+++ app/src/main/java/com/patrick/yangtubemusic/util/Constants.kt
@@ -30,26 +30,26 @@ object Constants {
             "https://lh3.googleusercontent.com/pixroKTrwwEDqLTP3wPA8ePRMvWBz4u4K_XVqeahLEEQ2ySdYXM_5x-qIt6PKB9S6eHzw_3vKcOURlCVCw=w120-h120-l90-rj"
         ),
         Music(
-            "내 기쁨은 네가 벤틀리를 타는 거야",
-            "김승민",
-            "https://lh3.googleusercontent.com/yZTtpy3PXhUx6BaUl-SxiDDqWvI-VfAGHvGpUfQOosEMqMstR10_9HecHd1u0w0vNrota6BGNk_HDg4=w120-h120-l90-rj",
+            "노래방에서",
+            "장범준",
+            "https://lh3.googleusercontent.com/fgN9gCK4kYnade5hhF6mrbjt4FJGsxi1NO4zM1EVEXkJS8D6cJBXPOPP-wq_t-gmZYOcp5WU80uFNHxZ9A=w120-h120-l90-rj",
             true
         ),
         Music(
-            "Me & You ◑",
-            "HONNE(혼네)",
-            "https://lh3.googleusercontent.com/XnLiu9JAt_Z4GDTeLlVQ1EVgFXS1xdQcVRDeh83whiC5TQgmboVuQsKmQU0L8Bx7uhqpgrpfZnnl1HFZ=w120-h120-l90-rj",
+            "아는사람 얘기",
+            "San E",
+            "https://lh3.googleusercontent.com/Yb6RcL7BT5rPNe8HDuD4AChseI3vAueWVYBAIbOiI_wcxv9rICitCuPOVPoi9JlotjW0RjS9fxj1HOw=w120-h120-l90-rj",
             false
         ),
         Music(
-            "니가 돌아올 희망은 없다는 걸 알아",
-            "경제환",
-            "https://lh3.googleusercontent.com/_At5xpPD0paZRRwN9KOLpnvc2yOwjWNVSBm6Es77cM6dC5sk15BKgsI9AQj8b97CyU-l1swqbOpPKkw=w120-h120-l90-rj"
+            "신호등",
+            "이무진",
+            "https://lh3.googleusercontent.com/o4jX-7hnRDn_RQkrYITMnP91k99QXW8CPcOYGWN4H8kB01eP56F2OUwDeZ-ohCS2P2h6SxgHhjpiY2gJ=w120-h120-l90-rj"
         ),
         Music(
-            "Tomorrow(feat. 기리보이(Giriboy), 빅나티)",
-            "lIlBOI",
-            "https://lh3.googleusercontent.com/pixroKTrwwEDqLTP3wPA8ePRMvWBz4u4K_XVqeahLEEQ2ySdYXM_5x-qIt6PKB9S6eHzw_3vKcOURlCVCw=w120-h120-l90-rj"
+            "헤어지지 못하는 여자, 떠나가지 못하는 남자 Can't Breakup Girl, Can't Breakaway Boy(feat. Jung In)",
+            "리쌍",
+            "https://lh3.googleusercontent.com/A0nTQjof_ndZo2etRfs185BDhGRgKrZqOx1O4wnm8GFQhNLlLSE73nXMb6w2AkroMOjSsVtpzGamUEle=w120-h120-l90-rj"
         ),
     )
 
@@ -80,60 +80,30 @@ object Constants {
         ),
         arrayOf(
             Music(
-                "니가 돌아올 희망은 없다는 걸 알아",
-                "경제환",
-                "https://lh3.googleusercontent.com/_At5xpPD0paZRRwN9KOLpnvc2yOwjWNVSBm6Es77cM6dC5sk15BKgsI9AQj8b97CyU-l1swqbOpPKkw=w120-h120-l90-rj"
+                "노래방에서",
+                "장범준",
+                "https://lh3.googleusercontent.com/fgN9gCK4kYnade5hhF6mrbjt4FJGsxi1NO4zM1EVEXkJS8D6cJBXPOPP-wq_t-gmZYOcp5WU80uFNHxZ9A=w120-h120-l90-rj",
+                true
             ),
             Music(
-                "Me & You ◑",
-                "HONNE(혼네)",
-                "https://lh3.googleusercontent.com/XnLiu9JAt_Z4GDTeLlVQ1EVgFXS1xdQcVRDeh83whiC5TQgmboVuQsKmQU0L8Bx7uhqpgrpfZnnl1HFZ=w120-h120-l90-rj"
+                "아는사람 얘기",
+                "San E",
+                "https://lh3.googleusercontent.com/Yb6RcL7BT5rPNe8HDuD4AChseI3vAueWVYBAIbOiI_wcxv9rICitCuPOVPoi9JlotjW0RjS9fxj1HOw=w120-h120-l90-rj",
+                false
             ),
             Music(
-                "Tomorrow(feat. 기리보이(Giriboy), 빅나티)",
-                "lIlBOI",
-                "https://lh3.googleusercontent.com/pixroKTrwwEDqLTP3wPA8ePRMvWBz4u4K_XVqeahLEEQ2ySdYXM_5x-qIt6PKB9S6eHzw_3vKcOURlCVCw=w120-h120-l90-rj"
+                "신호등",
+                "이무진",
+                "https://lh3.googleusercontent.com/o4jX-7hnRDn_RQkrYITMnP91k99QXW8CPcOYGWN4H8kB01eP56F2OUwDeZ-ohCS2P2h6SxgHhjpiY2gJ=w120-h120-l90-rj"
             ),
             Music(
-                "내 기쁨은 네가 벤틀리를 타는 거야",
-                "김승민",
-                "https://lh3.googleusercontent.com/yZTtpy3PXhUx6BaUl-SxiDDqWvI-VfAGHvGpUfQOosEMqMstR10_9HecHd1u0w0vNrota6BGNk_HDg4=w120-h120-l90-rj"
-            ),
-        ),
-        arrayOf(
-            Music(
-                "Me & You ◑",
-                "HONNE(혼네)",
-                "https://lh3.googleusercontent.com/XnLiu9JAt_Z4GDTeLlVQ1EVgFXS1xdQcVRDeh83whiC5TQgmboVuQsKmQU0L8Bx7uhqpgrpfZnnl1HFZ=w120-h120-l90-rj"
-            ),
-            Music(
-                "니가 돌아올 희망은 없다는 걸 알아",
-                "경제환",
-                "https://lh3.googleusercontent.com/_At5xpPD0paZRRwN9KOLpnvc2yOwjWNVSBm6Es77cM6dC5sk15BKgsI9AQj8b97CyU-l1swqbOpPKkw=w120-h120-l90-rj"
-            ),
-            Music(
-                "내 기쁨은 네가 벤틀리를 타는 거야",
-                "김승민",
-                "https://lh3.googleusercontent.com/yZTtpy3PXhUx6BaUl-SxiDDqWvI-VfAGHvGpUfQOosEMqMstR10_9HecHd1u0w0vNrota6BGNk_HDg4=w120-h120-l90-rj"
-            ),
-            Music(
-                "Tomorrow(feat. 기리보이(Giriboy), 빅나티)",
-                "lIlBOI",
-                "https://lh3.googleusercontent.com/pixroKTrwwEDqLTP3wPA8ePRMvWBz4u4K_XVqeahLEEQ2ySdYXM_5x-qIt6PKB9S6eHzw_3vKcOURlCVCw=w120-h120-l90-rj"
+                "헤어지지 못하는 여자, 떠나가지 못하는 남자 Can't Breakup Girl, Can't Breakaway Boy(feat. Jung In)",
+                "리쌍",
+                "https://lh3.googleusercontent.com/A0nTQjof_ndZo2etRfs185BDhGRgKrZqOx1O4wnm8GFQhNLlLSE73nXMb6w2AkroMOjSsVtpzGamUEle=w120-h120-l90-rj"
             ),
         ),
         arrayOf(
             Music(
-                "Tomorrow(feat. 기리보이(Giriboy), 빅나티)",
-                "lIlBOI",
-                "https://lh3.googleusercontent.com/pixroKTrwwEDqLTP3wPA8ePRMvWBz4u4K_XVqeahLEEQ2ySdYXM_5x-qIt6PKB9S6eHzw_3vKcOURlCVCw=w120-h120-l90-rj"
-            ),
-            Music(
-                "내 기쁨은 네가 벤틀리를 타는 거야",
-                "김승민",
-                "https://lh3.googleusercontent.com/yZTtpy3PXhUx6BaUl-SxiDDqWvI-VfAGHvGpUfQOosEMqMstR10_9HecHd1u0w0vNrota6BGNk_HDg4=w120-h120-l90-rj"
-            ),
-            Music(
                 "Me & You ◑",
                 "HONNE(혼네)",
                 "https://lh3.googleusercontent.com/XnLiu9JAt_Z4GDTeLlVQ1EVgFXS1xdQcVRDeh83whiC5TQgmboVuQsKmQU0L8Bx7uhqpgrpfZnnl1HFZ=w120-h120-l90-rj"
@@ -142,6 +112,40 @@ object Constants {
                 "니가 돌아올 희망은 없다는 걸 알아",
                 "경제환",
                 "https://lh3.googleusercontent.com/_At5xpPD0paZRRwN9KOLpnvc2yOwjWNVSBm6Es77cM6dC5sk15BKgsI9AQj8b97CyU-l1swqbOpPKkw=w120-h120-l90-rj"
+            ),
+            Music(
+                "내 기쁨은 네가 벤틀리를 타는 거야",
+                "김승민",
+                "https://lh3.googleusercontent.com/yZTtpy3PXhUx6BaUl-SxiDDqWvI-VfAGHvGpUfQOosEMqMstR10_9HecHd1u0w0vNrota6BGNk_HDg4=w120-h120-l90-rj"
+            ),
+            Music(
+                "Tomorrow(feat. 기리보이(Giriboy), 빅나티)",
+                "lIlBOI",
+                "https://lh3.googleusercontent.com/pixroKTrwwEDqLTP3wPA8ePRMvWBz4u4K_XVqeahLEEQ2ySdYXM_5x-qIt6PKB9S6eHzw_3vKcOURlCVCw=w120-h120-l90-rj"
+            ),
+        ),
+        arrayOf(
+            Music(
+                "아는사람 얘기",
+                "San E",
+                "https://lh3.googleusercontent.com/Yb6RcL7BT5rPNe8HDuD4AChseI3vAueWVYBAIbOiI_wcxv9rICitCuPOVPoi9JlotjW0RjS9fxj1HOw=w120-h120-l90-rj",
+                false
+            ),
+            Music(
+                "헤어지지 못하는 여자, 떠나가지 못하는 남자 Can't Breakup Girl, Can't Breakaway Boy(feat. Jung In)",
+                "리쌍",
+                "https://lh3.googleusercontent.com/A0nTQjof_ndZo2etRfs185BDhGRgKrZqOx1O4wnm8GFQhNLlLSE73nXMb6w2AkroMOjSsVtpzGamUEle=w120-h120-l90-rj"
+            ),
+            Music(
+                "신호등",
+                "이무진",
+                "https://lh3.googleusercontent.com/o4jX-7hnRDn_RQkrYITMnP91k99QXW8CPcOYGWN4H8kB01eP56F2OUwDeZ-ohCS2P2h6SxgHhjpiY2gJ=w120-h120-l90-rj"
+            ),
+            Music(
+                "노래방에서",
+                "장범준",
+                "https://lh3.googleusercontent.com/fgN9gCK4kYnade5hhF6mrbjt4FJGsxi1NO4zM1EVEXkJS8D6cJBXPOPP-wq_t-gmZYOcp5WU80uFNHxZ9A=w120-h120-l90-rj",
+                true
             ),
         ),
         arrayOf(
