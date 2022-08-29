6af84dc2aa967093b413e49ec01aa4d83b20c4e9
양태웅 <didxodnd0422@gmail.com>
2022-08-29 11:04:27 +0900

    Create Contents data class

---
+++ app/src/main/java/com/patrick/yangtubemusic/data/Contents.kt
@@ -0,0 +1,38 @@
+package com.patrick.yangtubemusic.data
+
+
+
+sealed class Contents(
+    val iconUrl: String? = null,
+    val contentTitle: String,
+    val contentDescription: String? = null,
+    val content: List<Content>
+) {
+    class ListenAgain(
+        contentTitle: String = "다시 듣기",
+        content: List<Content.Music>
+    ): Contents(
+        contentTitle = contentTitle,
+        content = content
+    )
+
+    class MixedForYou(
+        contentTitle: String = "맞춤 믹스",
+        content: List<Content.PlayList>
+    ): Contents(
+        contentTitle = contentTitle,
+        content = content
+    )
+
+    class ContentRecommendationByArtist(
+        thumbnailUrl: String,
+        contentTitle: String,
+        contentDescription: String = "아래 아티스트를 좋아한다면",
+        content: List<Content.Artist>
+    ): Contents(
+        iconUrl = thumbnailUrl,
+        contentTitle = contentTitle,
+        contentDescription = contentDescription,
+        content = content
+    )
+}
