8ef359e3bc65acaa6f556f3a50df5be0117a0b2b
양태웅 <didxodnd0422@gmail.com>
2022-08-31 17:24:38 +0900

    Create SearchFragment

---
+++ app/src/main/java/com/patrick/yangtubemusic/presentation/search/SearchFragment.kt
@@ -0,0 +1,11 @@
+package com.patrick.yangtubemusic.presentation.search
+
+import com.patrick.yangtubemusic.R
+import com.patrick.yangtubemusic.base.BaseFragment
+import com.patrick.yangtubemusic.databinding.FragmentSearchBinding
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    override fun init() {
+
+    }
+}
