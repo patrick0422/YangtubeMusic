5a8a42e4558149c628926be5fc406c2c48d2b344
양태웅 <didxodnd0422@gmail.com>
2022-10-17 13:59:42 +0900

    Implement color-extraction

--- app/src/main/java/com/patrick/yangtubemusic/presentation/common/player/CollapsiblePlayer.kt
+++ app/src/main/java/com/patrick/yangtubemusic/presentation/common/player/CollapsiblePlayer.kt
@@ -1,18 +1,23 @@
 package com.patrick.yangtubemusic.presentation.common.player
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
 import androidx.palette.graphics.Palette
+import coil.ImageLoader
 import coil.load
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.patrick.yangtubemusic.R
 import com.patrick.yangtubemusic.data.Content
 import com.patrick.yangtubemusic.databinding.PlayerCollapsableBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CollapsiblePlayer @JvmOverloads constructor(
     context: Context,
@@ -25,19 +30,29 @@ class CollapsiblePlayer @JvmOverloads constructor(
     fun setData(music: Content.Music) = with(binding) {
         imageThumbnail.load(music.thumbnailUrl)
         textTitle.text = music.title
+        textTitle.isSelected = true
         textSubtitle.text = music.artist
-
         minimizedTitle.text = music.title
         minimizedSubtitle.text = music.artist
 
         motionLayout.transitionToState(R.id.start)
 
-        val bitmap: Bitmap = (imageThumbnail.drawable as BitmapDrawable).bitmap
+        CoroutineScope(Dispatchers.IO).launch {
+            val loader = ImageLoader(context)
+            val request = ImageRequest.Builder(context)
+                .data(music.thumbnailUrl)
+                .allowHardware(false) // Disable hardware bitmaps.
+                .build()
 
-        Palette.Builder(bitmap).generate {
-            it?.let { palette ->
-                val dominantColor = palette.getDominantColor(resources.getColor(R.color.almost_black, null))
-                playerBackgroundView.setBackgroundColor(dominantColor)
+            val result = (loader.execute(request) as SuccessResult).drawable
+            val bitmap = (result as BitmapDrawable).bitmap
+
+            Palette.from(bitmap).generate() { palette ->
+                palette?.run {
+                    val color = ColorUtils.blendARGB((vibrantSwatch?.rgb ?: R.color.almost_black), Color.BLACK, 0.2F)
+
+                    playerBackgroundView.setBackgroundColor(color)
+                }
             }
         }
     }
